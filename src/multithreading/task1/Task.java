c40106043a17cfd48372e20962f3314e49cbcb7f
Maxim Yakimchikov <villelouis32@gmail.com>
2019-02-06 16:52:02 +0300

    Task1. Оптимизация - убраны ненужные переменные. Добавлено final поле callable - задачи, что не позволяет исполльзовать неинециированное конструктором ссылку на callable-объект.
    Signed-off-by: Maxim Yakimchikov <villelouis32@gmail.com>

--- src/multithreading/task1/Task.java
+++ src/multithreading/task1/Task.java
@@ -1,43 +1,36 @@
 package multithreading.task1;
+
 import java.util.concurrent.Callable;
 
-public class Task <T>{
-
-    private volatile boolean hasReuslt = false;
-    private volatile boolean hasError = false;
-    private Callable <? extends T> callable;
+public class Task<T> {
+    private final Callable<? extends T> callable;
     private volatile RuntimeException exception;
     private volatile T result;
 
-    public Task(Callable <? extends T> callable){
+    public Task(Callable<? extends T> callable) {
         this.callable = callable;
     }
 
-    public T get() throws Exception{
-        if (hasReuslt && !hasError){
+    public T get() throws Exception {
+        if ((result != null) && (exception == null)) {
             System.out.print("Has result  ");
             return result;
-        } else if (hasError){
+        } else if (exception != null) {
             throw exception;
-        } else {
-            synchronized (Task.class){
-                if (hasReuslt && !hasError){
-                    System.out.print("Has result  ");
-                    return result;
-                } else if (hasError){
-                    throw exception;
-                }
-                try {
-                    System.out.print("Computing  ");
-                    result = callable.call();
-                    hasReuslt = true;
-                    return result;
-                }
-                catch (Exception e){
-                    hasError = true;
-                    exception = new MultithreadingTaskException("Task was interrupted  ",e);
-                    throw exception;
-                }
+        } else synchronized (Task.class) {
+            if ((result != null) && (exception == null)) {
+                System.out.print("Has result  ");
+                return result;
+            } else if (exception != null) {
+                throw exception;
+            }
+            try {
+                System.out.print("Computing  ");
+                result = callable.call();
+                return result;
+            } catch (Exception e) {
+                exception = new MultithreadingTaskException("Task was interrupted  ", e);
+                throw exception;
             }
         }
     }
